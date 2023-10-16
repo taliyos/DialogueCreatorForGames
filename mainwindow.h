65df9b517523758ba5e7857cbb74bed95273290a
LoganGraham3 <loganildar01@gmail.com>
2023-10-16 16:05:27 -0400

    Revert "Merge branch 'main' into logan2"
    This reverts commit 62acdd0b65033622a0b2d78b7d2c509b3c630275, reversing
    changes made to e7376524b5edeff28000a8436e0aebad516bf1d7.

--- mainwindow.h
+++ mainwindow.h
@@ -15,12 +15,6 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
-    struct DialogueEntry {
-        QLineEdit* characterLabel;
-        QTextEdit* textBox;
-        QStringList effects;
-        bool autoState;
-    };
 private slots:
     void selectFont();
     void increaseFontSize();
@@ -41,17 +35,21 @@ private slots:
     QLineEdit* currentCharacter();
     bool currentAutoState();
     void updateAutoStateForCurrentText(bool state);
-    QString generateHtml(const DialogueEntry& entry);
-    void exportToHtml(const QString& filePath);
-    void onPreviewButtonClicked();
-    void selectBackgroundImage();
-
 
 private:
     Ui::MainWindow *ui;
     QString currentFile;
     QVBoxLayout *verticalLayout = new QVBoxLayout;
     QSplitter *splitter = new QSplitter(Qt::Vertical);
+
+    struct DialogueEntry {
+        QLineEdit* characterLabel;
+        QTextEdit* textBox;
+        QHBoxLayout* hLayout;
+        QVBoxLayout* columnLayout;
+        QStringList effects;
+        bool autoState;
+    };
     QList<DialogueEntry> textBoxes;
 
 
