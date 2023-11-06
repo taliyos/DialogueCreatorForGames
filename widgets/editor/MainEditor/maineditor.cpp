78926c9b82a49f97e8bc020fd474895308d0c560
LoganGraham3 <loganildar01@gmail.com>
2023-11-06 13:14:11 -0500

    ListFields load settings on creation

--- widgets/editor/MainEditor/maineditor.cpp
+++ widgets/editor/MainEditor/maineditor.cpp
@@ -262,6 +262,9 @@ void MainEditor::createListField() {
     // field is removed when the remove button is clicked within the UI.
     connect(listField, &ListField::removeField, this, &MainEditor::removeListField);
     connect(listField, &ListField::previewRequested, this, &MainEditor::handlePreviewRequest);
+
+    // update the list settings
+    editorTools->getListSettingsPage()->saveOptions();
 }
 
 void MainEditor::removeHead() {
